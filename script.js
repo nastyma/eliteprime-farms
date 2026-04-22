edd62998aea15c884d2005217a0981fb29ac361f
nastyma <140154153+nastyma@users.noreply.github.com>
2026-04-22 06:50:18 +0100

    Add script.js for website interactivity

---
+++ script.js
@@ -0,0 +1,10 @@
+// JavaScript code for ElitePrime Farms website interactivity
+
+// Display current date and time
+function displayDateTime() {
+    const now = new Date();
+    const dateTimeString = now.toUTCString();
+    console.log('Current Date and Time (UTC): ' + dateTimeString);
+}
+
+displayDateTime();
